8c4cedbcf4192c79a033eb45715d0c3edbca1a34
Raunak Khanna <raunaksachinkhanna@gmail.com>
2026-03-30 17:09:03 -0700

    Temporarily skip early pipeline stages for faster validation runs

--- main.py
+++ main.py
@@ -396,10 +396,10 @@ if __name__ == '__main__':
 
     if not args.train_str:
         if args.dataset == 'SoccerNet':
-            actions = {"soccer_ball_filter": True,
-                       "feat": True,
-                       "filter": True,
-                       "legible": True,
+            actions = {"soccer_ball_filter": False,
+                       "feat": False,
+                       "filter": False,
+                       "legible": False,
                        "legible_eval": False,
                        "pose": True,
                        "crops": True,
