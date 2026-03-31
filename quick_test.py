20b94d276f88c3ea48d7f00268ff67367d56f790
Hawakulu Abdullahi <hawakuludan@gmail.com>
2026-03-30 20:24:43 -0700

    added environment test script for dependency checking

---
+++ quick_test.py
@@ -0,0 +1,13 @@
+try:
+    import numpy as np
+    print("NumPy is installed")
+except:
+    print("NumPy is NOT installed")
+
+try:
+    import torch
+    print("PyTorch is installed")
+except:
+    print("PyTorch is NOT installed")
+
+print("Basic environment test complete")
